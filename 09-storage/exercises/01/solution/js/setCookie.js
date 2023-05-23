8f2864044588ea29101b6373da476e0b1c0d11bb
kovacseni <kovacseni@gmail.com>
2023-05-23 09:27:27 +0200

    bocs

--- 09-storage/exercises/01/solution/js/setCookie.js
+++ 09-storage/exercises/01/solution/js/setCookie.js
@@ -1,7 +1,7 @@
 
 function setCookie(text) {
     const expirationTime = new Date(Date.now() + (15 * 60 * 1000));
-    const expires = now.toUTCString();
+    const expires = expirationTime.toUTCString();
     document.cookie = `token=${text};expires=${expires}`;
 }
 
